d8950d404849e4f256f97990b1b4e8cee5ccb5ca
Chattiwa Kanarat <Chattiwa Kanarat>
2016-10-07 07:47:49 +0700

    pacman run

--- core/src/com/mygdx/pacman/GameScreen.java
+++ core/src/com/mygdx/pacman/GameScreen.java
@@ -9,39 +9,39 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
  
 public class GameScreen extends ScreenAdapter {
     private PacmanGame pacmanGame;
     private Texture pacmanImg;
-    private int x;
-    private int y;
+    private Pacman pacman;
     
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
         pacmanImg = new Texture("pacman.png");
-        x = 100;
-        y = 100;
+        pacman = new Pacman(100,100);
     }
     @Override
     public void render(float delta) {
         update(delta);
         SpriteBatch batch = pacmanGame.batch;
         batch.begin();
-        batch.draw(pacmanImg, x, y);
+        Vector2 pos = pacman.getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
         batch.end();
     }
     private void update(float delta) {
         if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+            pacman.move(Pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+            pacman.move(Pacman.DIRECTION_RIGHT);
         }
         if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-            y -= 10;
+            pacman.move(Pacman.DIRECTION_DOWN);
         }
         if(Gdx.input.isKeyPressed(Keys.UP)) {
-            y += 10;
+            pacman.move(Pacman.DIRECTION_UP);
         }
     }
 }
---
+++ core/src/com/mygdx/pacman/Pacman.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.pacman;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Pacman {
+    private Vector2 position;
+    public static final int DIRECTION_UP = 3;
+    public static final int DIRECTION_RIGHT = 2;
+    public static final int DIRECTION_DOWN = 1;
+    public static final int DIRECTION_LEFT = 4;
+    public static final int DIRECTION_STILL = 0;
+    private static final int [][] DIR_OFFSETS = new int [][] {
+        {0,0},
+        {0,-1},
+        {1,0},
+        {0,1},
+        {-1,0}
+    };
+    
+    public Pacman(int x, int y) {
+        position = new Vector2(x,y);
+    }    
+ 
+    public Vector2 getPosition() {
+        return position;    
+    }
+    
+    public void move(int dir) { 
+        position.x += 10 * DIR_OFFSETS[dir][0];
+        position.y += 10 * DIR_OFFSETS[dir][1];
+    }
+}
